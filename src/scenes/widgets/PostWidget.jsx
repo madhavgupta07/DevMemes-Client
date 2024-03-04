0c32e003d3671abf3ee54e40df1b3f51617c4b93
madhavgupta07 <madhavgupta5555@gmail.com>
2024-03-04 12:15:52 +0530

    first commit

---
+++ src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,124 @@
+import {
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+} from "@mui/icons-material";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost, setPosts } from "state";
+
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+}) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
+
+  const { palette } = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
+
+  const patchLike = async () => {
+    const response = await fetch(`https://devmemes-server.onrender.com/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `https://devmemes-server.onrender.com/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+  const deletePost = async () => {
+    try {
+      if (loggedInUserId !== postUserId) {
+        alert("You are not authorized to delete this post")
+        console.error("You are not authorized to delete this post");
+        return;
+      }
+      const response = await fetch(`https://devmemes-server.onrender.com/posts/delete/${postId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const updatedPost = await response.json();
+        dispatch(setPost({ post: updatedPost }));
+        getUserPosts()
+        // window.location.reload();
+      } else {
+        console.error("Failed to delete post");
+      }
+    } catch (error) {
+      console.error("Error deleting post:", error);
+    }
+  };
+
+  return (
+    <WidgetWrapper m="2rem 0">
+      <Friend
+        friendId={postUserId}
+        name={name}
+        subtitle={location}
+        userPicturePath={userPicturePath}
+      />
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
+      {picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`https://devmemes-server.onrender.com/assets/${picturePath}`}
+        />
+      )}
+      <FlexBetween mt="0.25rem">
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={patchLike}>
+              {isLiked ? (
+                <FavoriteOutlined sx={{ color: primary }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              )}
+            </IconButton>
+            <Typography>{likeCount}</Typography>
+        </FlexBetween>
+
+        <IconButton>
+          <DeleteIcon onClick={() => deletePost()}/>
+        </IconButton>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
+
+export default PostWidget;
